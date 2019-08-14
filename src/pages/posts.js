b0b6dc6ad40fec7698d12532d8b1660daa7e4e29
Aferguson17 <ashley.ferguson@sjsu.edu>
2019-08-13 23:34:29 -0700

    created site pages and some initial styles

---
+++ src/pages/posts.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default () => (
+    <div className="container">
+        <div style={{ paddingRight: "1em"}}>
+            <img
+                alt="placeholder"
+                className="image-card"
+                src="https://via.placeholder.com/150"
+            /> 
+        </div>
+    </div>
+)
+
