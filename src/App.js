2296b12cef289be0ae957132d7fd6714c4fa2139
Aferguson17 <ashley.ferguson@sjsu.edu>
2019-08-14 01:31:56 -0700

    started building out the navbar. Currently having issues with css loader

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import React from 'react';
+import './app.css'
 
-const App = () => (
-    <Container>
-        <Wrapper>
-            <Title>
-                Ferguson Capstone
-            </Title>
-        </Wrapper>
-    </Container>
-)
+function App() {
+    return (
+        <Root>
+            <header className="header">
+                <h1 className="header-title">Ferguson Capstone</h1>
+                <nav className="header-nav">
+                <link>Home</link>
+                <link>About</link>
+                <link>Contact Me</link>
+                </nav>
+            </header>
 
-export default App;
+            <div className="blog-content">
+             <p>Placeholder Text</p>
+            </div>
+
+            <div className="footer">
+            <span>Created by Ashley Ferguson 2019</span>
+            </div>
+        </Root>
+    )
+}
+
+export default App
